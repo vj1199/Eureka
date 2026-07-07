661a78fb1f82094914b47fbf5da8e72a79403e6e
stripe-payment-service-admin <janvj1169@gmail.com>
2026-07-07 12:57:05 +0530

    eureka application file is uploaded

---
+++ src/main/java/com/metaarch/eurekaserver/MetaArchEurekaServerApplication.java
@@ -0,0 +1,14 @@
+package com.metaarch.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class MetaArchEurekaServerApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(MetaArchEurekaServerApplication.class, args);
+    
+  }
+}
